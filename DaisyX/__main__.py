508fc7f8146ab26cbf834a7e42f09ee088bc6cb3
GurwinderBrar <79155513+Gurwinder262@users.noreply.github.com>
2021-03-03 17:33:44 +0530

    Update__main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -360,10 +360,10 @@ def DaisyX_about_callback(update, context):
     query = update.callback_query
     if query.data == "aboutmanu_":
         query.message.edit_text(
-            text=f"*😍 Hi again!  The name's {dispatcher.bot.first_name} 😍 \n\nAs  You I'm a next generational group management bot developed by Infinity_Bots.* "
-            f"\n\n 🔥 Join [Infinity_Bots](https://t.me/Infinity_Bots) To Keep Yourself Updated About {dispatcher.bot.first_name} 🔥"
+            text=f"* Hi again!  The name's {dispatcher.bot.first_name}  \n\n I'm a next generational group management bot developed by Magnificent Union.* "
+            f"\n\n 🔥 Join [Magnificent Union](https://t.me/MagnificentUnion) To Keep Yourself Updated About {dispatcher.bot.first_name} 🔥"
             f"\n\n I have the normal GROUP MANAGING functions like flood control, a warning system etc but I mainly have the advanced and handy Antispam system and the SIBYL banning system which safegaurds and helps your group from spammers."
-            f"\n\nI Can Manage Your Groups Smoothly, With Some Special Features [:)](https://telegra.ph/file/473cc17913393959e0667.jpg)"
+            f"\n\nI Can Manage Your Groups Smoothly, With Some Special Features [:)](https://telegra.ph/file/02cf4f4c7682fc5b5546c.jpg)"
             f"\n\n👇 You Can Know More About Me By Clicking The Below Buttons 👇",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
